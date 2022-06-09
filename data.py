a4b5bd6422e49375ecbf647085a6b5d119188b4b
smartkin2008 <40478476+smartkin2008@users.noreply.github.com>
2022-06-09 17:01:55 +0800

    上传data文件
    第一次使用上传文件功能

---
+++ data.py
@@ -0,0 +1,3 @@
+
+
+movie=['The Holy Grail',1975,'Terry Jones & Terry Gilliam',['The Holy Grailhell0',1975,'Terry Jones & Terry Gilliamhello',['The Holy Grailworld',1975,'Terry Jones & Terry Gilliamworld']]]
